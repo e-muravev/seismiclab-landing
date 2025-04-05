8ec986b3bffbcfbf4d6669f386af46a8f5a7ac69
Evgeny Muravev <emuravyev@issart.com>
2025-04-05 14:41:39 +0700

    initial

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../page";
+
+const Contacts = () => {
+  return (
+    <div className="relative fixed w-screen h-screen flex flex-col px-32">
+      <Header />
+      <main className="grow-1 z-1 flex items-center justify-center">Информация о данном разделе скоро появится.</main>
+    </div>
+  );
+};
+
+export default Contacts;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,140 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Fragment } from "react";
+
+export const headerLinks = [
+  { label: "Услуги", path: "services" },
+  { label: "Продукция", path: "products" },
+  { label: "Документация", path: "documents" },
+  { label: "О нас", path: "about" },
+  { label: "Контакты", path: "contacts" },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="relative fixed w-screen h-screen flex flex-col px-32">
+      <Header />
+      <main className="grow-1 z-1 pb-8">
+        <MainContent />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="relative basis-[200px] py-6">
+        <FindOutMoreButton />
       </footer>
+      <VideoWithOverlay />
     </div>
   );
 }
+
+export const Header = () => {
+  return (
+    <header className="basis-[112px] z-1 flex justify-between items-center py-6">
+      <Link href={"/"}>
+        <Image className="cursor-pointer" src="/logo.png" width={162} height={64} alt="logo" />
+      </Link>
+      <div>
+        {headerLinks.map((link) => (
+          <Link
+            href={link.path}
+            className="mr-4 cursor-pointer font-(family-name:--font-montserrat) text-lg hover:text-[#158df5]"
+            key={link.path}>
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+const MainContent = () => {
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div id="content" className="flex flex-col justify-end items-center text-justify grow-1 gap-32">
+        <div>
+          <h1 className="font-(family-name:--font-montserrat) text-6xl text-[#158df5] font-bold mb-4">
+            Шахтная и какая-то еще связь
+          </h1>
+          <p className="font-(family-name:--font-montserrat) text-3xl mb-4 text-justify w-1/2">
+            Мы располагаем профессиональным оборудованием и опытом, который гарантирует качество работы.
+          </p>
+          <p className="font-(family-name:--font-montserrat) text-xl text-right">
+            С января 2024 года являемся резидентом{" "}
+            <a className="underline decoration-solid hover:text-[#158df5]" href="https://www.sk.ru/">
+              «Сколково»
+            </a>
+            .
+          </p>
+        </div>
+        <div className="flex z-1 overflow-hidden ">
+          <div className="logobanner items-center gap-16">
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+            <Image src="/lukoil.png" alt="logo" width={200} height={102} />
+            <Image src="/rosneft.png" alt="logo" width={200} height={72} />
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+            <Image src="/lukoil.png" alt="logo" width={200} height={102} />
+            <Image src="/rosneft.png" alt="logo" width={200} height={72} />
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+            <Image src="/lukoil.png" alt="logo" width={200} height={102} />
+            <Image src="/rosneft.png" alt="logo" width={200} height={72} />
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+            <Image src="/lukoil.png" alt="logo" width={200} height={102} />
+            <Image src="/rosneft.png" alt="logo" width={200} height={72} />
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+            <Image src="/lukoil.png" alt="logo" width={200} height={102} />
+            <Image src="/rosneft.png" alt="logo" width={200} height={72} />
+            <Image src="/gazprom.png" alt="logo" width={200} height={102} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const FindOutMoreButton = () => {
+  return (
+    <Link href="about">
+      <button className="absolute right-0 bottom-6 w-64 h-16 bg-[#158df5] z-1 flex items-center justify-center hover:bg-sky-700 cursor-pointer">
+        <p className="font-(family-name:--font-montserrat) text-xl mr-4 cursor-pointer">Узнать больше</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="37" height="29" viewBox="0 0 37 29" fill="none">
+          <g opacity="0.5">
+            <mask id="path-1-inside-1_5135_28368" fill="white">
+              <path d="M1.57887 13.061C1.15978 13.061 0.757862 13.2275 0.461521 13.5239C0.165184 13.8202 -0.00129698 14.2221 -0.00129698 14.6412C-0.00129697 15.0603 0.165184 15.4622 0.461521 15.7585C0.757862 16.0549 1.15978 16.2214 1.57887 16.2214L1.57887 13.061ZM1.57887 16.2214L35.2891 16.2214L35.2891 13.061L1.57887 13.061L1.57887 16.2214Z" />
+            </mask>
+            <path
+              d="M1.57887 13.061C1.15978 13.061 0.757862 13.2275 0.461521 13.5239C0.165184 13.8202 -0.00129698 14.2221 -0.00129698 14.6412C-0.00129697 15.0603 0.165184 15.4622 0.461521 15.7585C0.757862 16.0549 1.15978 16.2214 1.57887 16.2214L1.57887 13.061ZM1.57887 16.2214L35.2891 16.2214L35.2891 13.061L1.57887 13.061L1.57887 16.2214Z"
+              fill="black"
+            />
+            <path
+              d="M3.74553 16.2214L3.74553 13.061L-0.587799 13.061L-0.587799 16.2214L3.74553 16.2214ZM35.2891 16.2214L35.2891 20.5547L39.6224 20.5547L39.6224 16.2214L35.2891 16.2214ZM35.2891 13.061L39.6224 13.061L39.6224 8.7277L35.2891 8.7277L35.2891 13.061ZM1.57887 8.7277C0.0105094 8.7277 -1.49361 9.35073 -2.60261 10.4597L3.52565 16.588C3.00933 17.1043 2.30906 17.3944 1.57887 17.3944L1.57887 8.7277ZM-2.60261 10.4597C-3.71161 11.5687 -4.33463 13.0728 -4.33463 14.6412L4.33204 14.6412C4.33204 15.3714 4.04198 16.0717 3.52565 16.588L-2.60261 10.4597ZM-4.33463 14.6412C-4.33463 16.2096 -3.71161 17.7137 -2.60261 18.8227L3.52565 12.6944C4.04198 13.2107 4.33204 13.911 4.33204 14.6412L-4.33463 14.6412ZM-2.60261 18.8227C-1.49361 19.9317 0.0105096 20.5547 1.57887 20.5547L1.57887 11.888C2.30906 11.888 3.00933 12.1781 3.52565 12.6944L-2.60261 18.8227ZM1.57887 20.5547L35.2891 20.5547L35.2891 11.888L1.57887 11.888L1.57887 20.5547ZM39.6224 16.2214L39.6224 13.061L30.9557 13.061L30.9557 16.2214L39.6224 16.2214ZM35.2891 8.7277L1.57887 8.7277L1.57887 17.3944L35.2891 17.3944L35.2891 8.7277Z"
+              fill="white"
+              mask="url(#path-1-inside-1_5135_28368)"
+            />
+          </g>
+          <path
+            d="M22.6477 27.2826L35.2891 14.6413L22.6477 2"
+            stroke="white"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+    </Link>
+  );
+};
+
+const VideoWithOverlay = () => {
+  return (
+    <Fragment>
+      <video
+        className="absolute left-0 top-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
+        src="/videos/mainPageVideo.mp4">
+        Извините ваш браузер не поддерживает видео
+      </video>
+      <div className="absolute left-0 top-0 w-full h-full bg-black/50" />
+    </Fragment>
+  );
+};
